92fe8c95630501f80295f80940aa4fd2de8e1740
PriyanshuRanjann <14priyanshuranjan@gmail.com>
2026-01-08 16:57:00 +0530

    feat : add fibonacci using  recursion

--- fibonacci.py
+++ fibonacci.py
@@ -9,7 +9,23 @@ def fibonacci(n: int):
         print(f"An error occurred: {e}")
     return lst
 
+def fibonacci_recursion(n):
+    try:
+        a=0
+        b=1
+        if n <=0:
+            return []
+        elif n == 1:
+            return [a]
+        elif n == 2:
+            return [a, b]
+        else:
+            lst = fibonacci_recursion(n - 1)
+            lst.append(lst[-1] + lst[-2])
+            return lst
+    except RecursionError as e:
+        return e
 
 if __name__ == "__main__":
     num = int(input("Enter the number of terms in Fibonacci sequence: "))
-    print(f"Fibonacci sequence with {num} terms: {fibonacci(num)}")
+    print(f"Fibonacci sequence with {num} terms: {fibonacci_recursion(num)}")
